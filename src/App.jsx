6e2f17bd60b9d07b74f728f030029bf5bb513f7f
Victor Feliciano <victorjfeliciano@gmail.com>
2023-05-16 06:35:23 -0300

    first integration with cms

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useQuery } from 'graphql-hooks';
 import './App.css'
+import { StructuredText } from 'react-datocms/structured-text';
+
+const HOMEPAGE_QUERY = `query Article($limit: IntType) {
+  allStories(first: $limit) {
+    id
+    title
+    preview
+  }
+}`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loading, error, data } = useQuery(HOMEPAGE_QUERY, {
+    variables: {
+      limit: 10
+    }
+  });
+
+  if (loading) return "Loading...";
+  if (error) return "Something Bad Happened";
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        {data.allStories.map(article => (
+          <article>
+            <h6>{article.title}</h6>
+            <p>{article.preview}</p>
+          </article>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ClientContext, GraphQLClient } from 'graphql-hooks';
+
+const client = new GraphQLClient({
+  url: "https://graphql.datocms.com/",
+  headers: {
+    "Authorization": `Bearer ${import.meta.env.VITE_DATOCMS_API_KEY}`,
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ClientContext.Provider value={client}>
+      <App />
+    </ClientContext.Provider>
   </React.StrictMode>,
 )
